0b326cd5dd4729186d694546817ef38171c4a616
suwit <ssupawas@gmail.com>
2019-05-27 10:07:58 +0700

    Var and Constant

---
+++ VarAndConstant.playground/Contents.swift
@@ -0,0 +1,20 @@
+//การประกาศตัวแปรแบบกำหนดค่าได้
+var myName = "สุวิทย์" //นี่คือการประกาศตัวแปรแบบใช้ value เป็นตัวกำหนด data type
+var myCompany: String = "FEBC"  //นี่คือการประกาศตัวแปรแบบกำหนด datatype เลย
+
+var myNumber = 123 //For Integer
+var myNumber2: Int = 456 // Assign datatype
+
+var myNumber3 = 12.34 //For Double
+var myNumber4: Double = 45.67 //Assign Datatype
+
+var status = true //For Booleane  ค่ามีถูกผิด
+var status2: Bool = false  //Assign Datatype
+
+
+
+
+
+
+
+
